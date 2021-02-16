0d31a457efa93d42bbaa518973a37a50fdb249e6
Bernardo Pereira Oliveira <bernardo.258@hotmail.com>
2021-02-15 23:38:37 -0300

    Refatorando o código SASS e adicionando um Readme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Wrapper from './components/Wrapper/Wrapper';
 import Header from './components/Header/Header';
 import UltimoProjeto from './components/UltimoProjeto/UltimoProjeto';
 
-// import "./main.scss"
+import "./main.scss"
 
 function App() {
   return (
